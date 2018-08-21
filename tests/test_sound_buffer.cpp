a53372be9629dc0abd1d112c71b74aecd3f51721
Elias Farhan <elias.farhan@gmail.com>
2018-08-21 13:45:12 +0200

    Adding google test and the sound buffer test

---
+++ tests/test_sound_buffer.cpp
@@ -0,0 +1,76 @@
+/*
+MIT License
+
+Copyright (c) 2017 SAE Institute Switzerland AG
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include <sstream>
+#include <engine/engine.h>
+#include <audio/audio.h>
+#include <audio/sound.h>
+#include <engine/log.h>
+int main()
+{
+	sfge::Engine engine;
+	auto& soundBufferManager = engine.GetAudioManager().GetSoundBufferManager();
+
+	const std::string goodSoundBufferPath = "data/sounds/doorClose_1.ogg";
+	const std::string badTextPath = "fake/path/prout.ogg";
+	const std::string badTextPathWithoutExtension = "fake/path/prout";
+
+	const auto goodSndBufferId = soundBufferManager.LoadSoundBuffer(goodSoundBufferPath);
+	const auto badSndBufferId = soundBufferManager.LoadSoundBuffer(badTextPath);
+	const auto badExtSndBufferId = soundBufferManager.LoadSoundBuffer(badTextPathWithoutExtension);
+
+
+	sf::Sound sound;
+	if (badSndBufferId != sfge::INVALID_TEXTURE)
+	{
+		sfge::Log::GetInstance()->Error("Loading Bad File");
+		sound.setBuffer(*soundBufferManager.GetSoundBuffer(badSndBufferId));
+	}
+	else
+	{
+		std::ostringstream oss;
+		oss << "Bad file: " << badTextPath << " could not be loaded";
+		sfge::Log::GetInstance()->Error(oss.str());
+	}
+	if (goodSndBufferId != sfge::INVALID_TEXTURE)
+	{
+
+		sfge::Log::GetInstance()->Msg("Loading Good File");
+		sound.setBuffer(*soundBufferManager.GetSoundBuffer(goodSndBufferId));
+	}
+	else
+	{
+		std::ostringstream oss;
+		oss << "Bad file: " << goodSoundBufferPath << " could not be loaded";
+		sfge::Log::GetInstance()->Error(oss.str());
+	}
+
+	sound.play();
+
+	std::this_thread::sleep_for(std::chrono::milliseconds
+	(soundBufferManager.GetSoundBuffer(goodSndBufferId)->getDuration().asMilliseconds()));
+#if WIN32
+	system("pause");
+#endif
+	return EXIT_SUCCESS;
+}
