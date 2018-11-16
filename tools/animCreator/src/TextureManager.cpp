5358ac1d702921bdf05b340199a8d1b22f1023e1
Duncan Bourquard <bourquardduncan@gmail.com>
2018-11-16 14:35:16 +0100

    Adding MIT license and correcting typo.

--- tools/animCreator/src/TextureManager.cpp
+++ tools/animCreator/src/TextureManager.cpp
@@ -1,3 +1,32 @@
+/*
+MIT License
+
+Copyright (c) 2018 SAE Institute Switzerland AG
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------
+Author : Duncan Bourquard (bourquardduncan@gmail.com)
+Date : November 2018
+Project : AnimationTool for SFGE
+*/
+
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
@@ -16,7 +45,7 @@ bool TextureManager::LoadTexture(std::string path, int sizeX, int sizeY)
 	return LoadTexture(path, sizeX, sizeY, 0, 0);
 }
 
-bool TextureManager::LoadTexture(std::string path, int SizeX, int SizeY, int numRow, int numCol)
+bool TextureManager::LoadTexture(std::string path, int sizeX, int sizeY, int numRow, int numCol)
 {
 	sf::Texture newTexture;
 	if (newTexture.loadFromFile(path))
@@ -39,8 +68,8 @@ bool TextureManager::LoadTexture(std::string path, int SizeX, int SizeY, int num
 				}
 				newTextureInfo->fileName = fileName;
 
-				newTextureInfo->position = sf::Vector2i(i*SizeX, j*SizeY);
-				newTextureInfo->size = sf::Vector2i(SizeX, SizeY);;
+				newTextureInfo->position = sf::Vector2i(i*sizeX, j*sizeY);
+				newTextureInfo->size = sf::Vector2i(sizeX, sizeY);;
 				newTextureInfo->id = m_lastId;
 				m_lastId++;
 				m_textures.push_back(newTextureInfo);
