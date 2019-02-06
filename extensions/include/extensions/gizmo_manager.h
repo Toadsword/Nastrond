d20231149774739d74bb4b6431f71c8bf079570e
MarSaturn <tristan.schorr@hotmail.fr>
2019-02-06 11:38:53 +0100

    add gizmo .cpp and .h in extensions to try draw function

---
+++ extensions/include/extensions/gizmo_manager.h
@@ -0,0 +1,153 @@
+/*
+MIT License
+
+Copyright (c) 2017 SAE Institute Switzerland AG
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef GIZMO_MANAGER_H
+#define GIZMO_MANAGER_H
+
+#include <engine/system.h>
+#include <engine/transform2d.h>
+#include <graphics/graphics2d.h>
+
+#include <extensions/building_utilities.h>
+
+namespace sfge::ext
+{
+//#define DEBUG_CHECK_PRODUCTION
+#define TEST_SYSTEM_DEBUG
+
+	/**
+	 * \author Tristan Schorr
+	 */
+	class GizmoManager : public System
+	{
+	public:
+		GizmoManager(Engine& engine);
+
+		void Init() override;
+
+		void Update(float dt) override;
+
+		void FixedUpdate() override;
+
+		void Draw() override;
+
+		/**
+		 * \brief Spawn forge entity at the given position.
+		 */
+		void AddNewBuilding(Vec2f pos);
+
+		/**
+		 * \brief Destroy the forge at the given index. Return if the entity is not available.
+		 */
+		bool DestroyBuilding(Entity entity);
+
+		/**
+		 * \brief Add dwarf to the dwarf slot struct of the given entity. Return false if the entity do not exist.
+		 */
+		bool AddDwarfToBuilding(Entity mineEntity);
+
+		/**
+		 * \brief Remove dwarf to the dwarf slot struct of the given entity. Return false if there is no dwarf attribute or if the entity do not exist.
+		 */
+		bool RemoveDwarfToBuilding(Entity mineEntity);
+
+		/**
+		 * \brief Return a forge entity with a slot available for a dwarf. If not
+		 */
+		Entity GetFreeSlotInBuilding();
+
+		/**
+		 * \brief Return the type of resources that the building produce.
+		 */
+		ResourceType GetProducedResourceType();
+
+		/**
+		 * \brief Return a vector with all the type of resources that the building need to produce.
+		 */
+		std::vector<ResourceType> GetNeededResourceType();
+
+		/**
+		 * \brief Return an amount of the produced resources of the given building entity.
+		 */
+		int GetResourcesBack(Entity entity);
+
+		/**
+		 * \brief Give resources to the the given building entity. Return the number of resources that the building could not recover or all if the building is full or if the resource type is not the good one.
+		 */
+		float GiveResources(Entity entity, int nmbResources, ResourceType resourceType);
+
+	private:
+		/**
+		 * \brief Resize all vector in one go to keep the synchronize all index.
+		 */
+		void ResizeContainer(const size_t newSize);
+
+		/**
+		 * \brief Consume iron to produce tool.
+		 */
+		void ProduceTools();
+
+		/**
+		 * \brief return true if a slot in the index is empty then take his place.
+		 */
+		bool CheckEmptySlot(Entity newEntity, Transform2d* transformPtr);
+
+		Transform2dManager* m_Transform2DManager;
+		TextureManager* m_TextureManager;
+		SpriteManager* m_SpriteManager;
+
+#ifdef TEST_SYSTEM_DEBUG
+		const size_t m_EntitiesNmb = 10000;
+		size_t m_EntitiesCount = 0;
+
+		const unsigned int m_FramesBeforeAdd = 0u;
+		unsigned int m_FrameInProgress = 0u;
+#endif
+
+		std::vector<Entity> m_EntityIndex;
+
+		std::vector<DwarfSlots> m_DwarfSlots;
+		std::vector<GiverInventory> m_ToolsInventories;
+		std::vector<ReceiverInventory> m_IronsInventories;
+		std::vector<ProgressionProduction> m_ProgressionProdTool;
+
+		const int m_stackSize = 5;
+
+		const int m_CoolDownFrames = 20;
+
+		ResourceType m_ResourceTypeNeeded = ResourceType::IRON;
+		ResourceType m_ResourceTypeProduced = ResourceType::TOOL;
+
+
+		//Building texture
+		std::string m_TexturePath;
+		TextureId m_TextureId;
+		sf::Texture* m_Texture;
+
+		//Vertex array
+		sf::VertexArray m_VertexArray;
+	};
+}
+#endif
+
---
+++ extensions/src/gizmo_manager.cpp
@@ -0,0 +1,326 @@
+/*
+MIT License
+
+Copyright (c) 2017 SAE Institute Switzerland AG
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <extensions/gizmo_manager.h>
+
+
+namespace sfge::ext
+{
+	GizmoManager::GizmoManager(Engine& engine) : System(engine) {}
+
+	void GizmoManager::Init()
+	{
+		m_Transform2DManager = m_Engine.GetTransform2dManager();
+		m_TextureManager = m_Engine.GetGraphics2dManager()->GetTextureManager();
+		m_SpriteManager = m_Engine.GetGraphics2dManager()->GetSpriteManager();
+
+		//Load Texture
+		m_TexturePath = "data/sprites/building.png";
+		m_TextureId = m_TextureManager->LoadTexture(m_TexturePath);
+		m_Texture = m_TextureManager->GetTexture(m_TextureId);
+
+		m_VertexArray = sf::VertexArray(sf::Quads, 0);
+	}
+
+	void GizmoManager::Update(float dt){}
+
+	void GizmoManager::FixedUpdate()
+	{
+		ProduceTools();
+
+#ifdef TEST_SYSTEM_DEBUG
+		m_FrameInProgress++;
+		if (m_FrameInProgress >= m_FramesBeforeAdd && m_EntitiesNmb > m_EntitiesCount)
+		{
+			m_EntitiesCount++;
+			AddNewBuilding(Vec2f(std::rand() % static_cast<int>(1280), std::rand() % static_cast<int>(720)));
+			m_FrameInProgress = 0;
+		}
+#endif
+	}
+
+	void GizmoManager::Draw()
+	{
+		auto window = m_Engine.GetGraphics2dManager()->GetWindow();
+
+		window->draw(m_VertexArray, m_Texture);
+	}
+
+	void GizmoManager::AddNewBuilding(Vec2f pos)
+	{
+		auto* entityManager = m_Engine.GetEntityManager();
+
+		Configuration* configuration = m_Engine.GetConfig();
+		entityManager->ResizeEntityNmb(configuration->currentEntitiesNmb + 1);
+
+		const auto newEntity = entityManager->CreateEntity(0);
+
+		//add transform
+		auto transformPtr = m_Transform2DManager->AddComponent(newEntity);
+		transformPtr->Position = Vec2f(pos.x, pos.y);
+
+		if (!CheckEmptySlot(newEntity, transformPtr))
+		{
+			size_t newForge = m_EntityIndex.size();
+
+
+			ResizeContainer(newForge + 1);
+
+			m_IronsInventories[newForge].resourceType = ResourceType::IRON;
+			m_ToolsInventories[newForge].resourceType = ResourceType::TOOL;
+			m_ProgressionProdTool[newForge].resourceType = ResourceType::TOOL;
+
+			m_EntityIndex[newForge] = newEntity;
+
+			const sf::Vector2f textureSize = sf::Vector2f(m_Texture->getSize().x, m_Texture->getSize().y);
+
+			m_VertexArray[4 * newForge].texCoords = sf::Vector2f(0, 0);
+			m_VertexArray[4 * newForge + 1].texCoords = sf::Vector2f(textureSize.x, 0);
+			m_VertexArray[4 * newForge + 2].texCoords = textureSize;
+			m_VertexArray[4 * newForge + 3].texCoords = sf::Vector2f(0, textureSize.y);
+
+			m_VertexArray[4 * newForge].position = transformPtr->Position - textureSize / 2.0f;
+			m_VertexArray[4 * newForge + 1].position = transformPtr->Position + sf::Vector2f(textureSize.x / 2.0f, -textureSize.y / 2.0f);
+			m_VertexArray[4 * newForge + 2].position = transformPtr->Position + textureSize / 2.0f;
+			m_VertexArray[4 * newForge + 3].position = transformPtr->Position + sf::Vector2f(-textureSize.x / 2.0f, textureSize.y / 2.0f);
+
+		}
+	}
+
+	bool GizmoManager::DestroyBuilding(Entity entity)
+	{
+		for (int i = 0; i < m_EntityIndex.size(); i++)
+		{
+			if (m_EntityIndex[i] == entity)
+			{
+				m_EntityIndex[i] = INVALID_ENTITY;
+				EntityManager* entityManager = m_Engine.GetEntityManager();
+				entityManager->DestroyEntity(entity);
+
+				sf::Vector2f resetSize = sf::Vector2f(0, 0);
+				m_VertexArray[4 * i].texCoords = resetSize;
+				m_VertexArray[4 * i + 1].texCoords = resetSize;
+				m_VertexArray[4 * i + 2].texCoords = resetSize;
+				m_VertexArray[4 * i + 3].texCoords = resetSize;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool GizmoManager::AddDwarfToBuilding(Entity entity)
+	{
+		for (int i = 0; i < m_EntityIndex.size(); i++)
+		{
+			if (m_EntityIndex[i] == entity)
+			{
+				if (m_DwarfSlots[i].dwarfAttributed < m_DwarfSlots[i].maxDwarfCapacity)
+				{
+					m_DwarfSlots[i].dwarfAttributed++;
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			return false;
+		}
+	}
+
+	bool GizmoManager::RemoveDwarfToBuilding(Entity entity)
+	{
+		for (int i = 0; i < m_EntityIndex.size(); i++)
+		{
+			if (m_EntityIndex[i] == entity)
+			{
+				m_DwarfSlots[i].dwarfAttributed--;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	Entity GizmoManager::GetFreeSlotInBuilding()
+	{
+		for (int i = 0; i < m_DwarfSlots.size(); i++)
+		{
+			if (m_DwarfSlots[i].dwarfAttributed < m_DwarfSlots[i].maxDwarfCapacity)
+			{
+				return m_EntityIndex[i];
+			}
+		}
+		return INVALID_ENTITY;
+	}
+
+	ResourceType GizmoManager::GetProducedResourceType()
+	{
+		return m_ResourceTypeProduced;
+	}
+
+	std::vector<ResourceType> GizmoManager::GetNeededResourceType()
+	{
+		std::vector<ResourceType> resourceTypes;
+		resourceTypes.push_back(m_ResourceTypeNeeded);
+		return resourceTypes;
+	}
+
+	int GizmoManager::GetResourcesBack(Entity entity)
+	{
+
+		for (unsigned int i = 0; i < m_EntityIndex.size(); i++)
+		{
+			if (m_EntityIndex[i] == entity)
+			{
+				m_ToolsInventories[i].packNumber--;
+				return m_ToolsInventories[i].packSize;
+			}
+		}
+		return 0;
+	}
+
+	float GizmoManager::GiveResources(Entity entity, int nmbResources, ResourceType resourceType)
+	{
+		for (unsigned int i = 0; i < m_EntityIndex.size(); i++)
+		{
+			if (m_EntityIndex[i] == entity)
+			{
+				if(m_IronsInventories[i].resourceType == resourceType)
+				{
+					m_IronsInventories[i].inventory += nmbResources;
+					float tmpResourcesExcess = m_IronsInventories[i].inventory - m_IronsInventories[i].maxCapacity;
+
+					if(tmpResourcesExcess > 0)
+					{
+						return tmpResourcesExcess;
+					}
+				}
+				else
+				{
+					return nmbResources;
+				}
+				
+			}
+		}
+		return nmbResources;
+	}
+
+	void GizmoManager::ResizeContainer(const size_t newSize)
+	{
+		m_DwarfSlots.resize(newSize);
+		m_EntityIndex.resize(newSize);
+		m_ToolsInventories.resize(newSize);
+		m_IronsInventories.resize(newSize);
+		m_ProgressionProdTool.resize(newSize);
+		m_VertexArray.resize(newSize * 4);
+
+	}
+
+	void GizmoManager::ProduceTools()
+	{
+		for (int i = 0; i < m_EntityIndex.size(); i++)
+		{
+			if (m_EntityIndex[i] == NULL)
+			{
+				continue;
+			}
+
+			if (m_IronsInventories[i].inventory <= 0 || m_ToolsInventories[i].inventory + m_ToolsInventories[i].packNumber * m_ToolsInventories[i].packSize >= m_ToolsInventories[i].maxCapacity)
+			{
+				continue;
+			}
+
+			m_ProgressionProdTool[i].FrameCoolDown += m_DwarfSlots[i].dwarfIn;
+
+			if (m_ProgressionProdTool[i].FrameCoolDown >= m_CoolDownFrames)
+			{
+				m_IronsInventories[i].inventory--;
+				m_ProgressionProdTool[i].progression++;
+				m_ProgressionProdTool[i].FrameCoolDown = 0;
+			}
+
+			if (m_ProgressionProdTool[i].progression >= m_ProgressionProdTool[i].goal)
+			{
+				m_ToolsInventories[i].inventory++;
+				m_ProgressionProdTool[i].progression = 0;
+
+				if (m_ToolsInventories[i].inventory >= m_ToolsInventories[i].packSize)
+				{
+					m_ToolsInventories[i].packNumber++;
+					m_ToolsInventories[i].inventory -= m_ToolsInventories[i].packSize;
+				}
+			}
+
+#ifdef DEBUG_CHECK_PRODUCTION
+
+			std::cout << "Iron Inventory of mine " + std::to_string(i + 1) + " : " + std::to_string(m_ToolsInventories[i].inventory) +
+				" / and pack Number : " + std::to_string(m_ToolsInventories[i].packNumber) + " / progression : " + std::to_string(m_ProgressionProdTool[i].progression) + "\n";
+			if (i + 1 == m_entitiesNmb)
+			{
+				std::cout << "\n";
+			}
+#endif
+		}
+	}
+
+	bool GizmoManager::CheckEmptySlot(Entity newEntity, Transform2d* transformPtr)
+	{
+		for (int i = 0; i < m_EntityIndex.size(); i++)
+		{
+			if (m_EntityIndex[i] == NULL)
+			{
+				m_EntityIndex[i] = newEntity;
+				const DwarfSlots newDwarfSlot;
+				m_DwarfSlots[i] = newDwarfSlot;
+				const ReceiverInventory newIronInventory;
+				m_IronsInventories[i] = newIronInventory;
+				const GiverInventory newToolInventory;
+				m_ToolsInventories[i] = newToolInventory;
+				const ProgressionProduction newProgressionProdTool;
+				m_ProgressionProdTool[i] = newProgressionProdTool;
+
+				m_IronsInventories[i].resourceType = ResourceType::IRON;
+				m_ToolsInventories[i].resourceType = ResourceType::TOOL;
+				m_ProgressionProdTool[i].resourceType = ResourceType::TOOL;
+
+				m_ToolsInventories[i].packSize = m_stackSize;
+
+				const sf::Vector2f textureSize = sf::Vector2f(m_Texture->getSize().x, m_Texture->getSize().y);
+
+				m_VertexArray[4 * i].texCoords = sf::Vector2f(0, 0);
+				m_VertexArray[4 * i + 1].texCoords = sf::Vector2f(textureSize.x, 0);
+				m_VertexArray[4 * i + 2].texCoords = textureSize;
+				m_VertexArray[4 * i + 3].texCoords = sf::Vector2f(0, textureSize.y);
+
+				m_VertexArray[4 * i].position = transformPtr->Position - textureSize / 2.0f;
+				m_VertexArray[4 * i + 1].position = transformPtr->Position + sf::Vector2f(textureSize.x / 2.0f, -textureSize.y / 2.0f);
+				m_VertexArray[4 * i + 2].position = transformPtr->Position + textureSize / 2.0f;
+				m_VertexArray[4 * i + 3].position = transformPtr->Position + sf::Vector2f(-textureSize.x / 2.0f, textureSize.y / 2.0f);
+
+				return true;
+			}
+		}
+		return false;
+	}
+}
